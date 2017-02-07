17f43f9a430e1b9cb28d9b090b60a6de34905e16
Ruslan Koval <ruslan.koval21@gmail.com>
2017-02-07 14:18:49 +0200

    basic MVC structure

---
+++ lib/base/Model.php
@@ -0,0 +1,123 @@
+<?php
+
+class Model
+{
+	protected $_dbh = null;
+	protected $_table = "";
+	
+	public function __construct()
+	{
+		// parses the settings file
+		$settings = parse_ini_file(ROOT_PATH . '/config/settings.ini', true);
+		
+		// starts the connection to the database
+		$this->_dbh = new PDO(
+			sprintf(
+				"%s:host=%s;dbname=%s",
+				$settings['database']['driver'],
+				$settings['database']['host'],
+				$settings['database']['dbname']
+			),
+			$settings['database']['user'],
+			$settings['database']['password'],
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+		);
+		
+		$this->init();
+	}
+	
+	public function init()
+	{
+		
+	}
+
+    /**
+     * @param $table
+     */
+    protected function _setTable($table)
+	{
+		$this->_table = $table;
+	}
+	
+	public function fetchOne($id)
+	{
+		$sql = 'select * from ' . $this->_table;
+		$sql .= ' where id = ?';
+		
+		$statement = $this->_dbh->prepare($sql);
+		$statement->execute(array($id));
+		
+		return $statement->fetch(PDO::FETCH_OBJ);
+	}
+
+    /**
+     * @param array $data
+     * @return bool|string
+     */
+    public function save($data = array())
+	{
+		$sql = '';
+		
+		$values = array();
+		
+		if (array_key_exists('id', $data)) {
+			$sql = 'update ' . $this->_table . ' set ';
+			
+			$first = true;
+			foreach($data as $key => $value) {
+				if ($key != 'id') {
+					$sql .= ($first == false ? ',' : '') . ' ' . $key . ' = ?';
+					
+					$values[] = $value;
+					
+					$first = false;
+				}
+			}
+			
+			// adds the id as well
+			$values[] = $data['id'];
+			
+			$sql .= ' where id = ?';// . $data['id'];
+			
+			$statement = $this->_dbh->prepare($sql);
+			return $statement->execute($values);
+		}
+		else {
+			$keys = array_keys($data);
+			
+			$sql = 'insert into ' . $this->_table . '(';
+			$sql .= implode(',', $keys);
+			$sql .= ')';
+			$sql .= ' values (';
+			
+			$dataValues = array_values($data);
+			$first = true;
+			foreach($dataValues as $value) {
+				$sql .= ($first == false ? ',?' : '?');
+				
+				$values[] = $value;
+				
+				$first = false;
+			}
+			
+			$sql .= ')';
+			
+			$statement = $this->_dbh->prepare($sql);
+			if ($statement->execute($values)) {
+				return $this->_dbh->lastInsertId();
+			}
+		}
+		
+		return false;
+	}
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function delete($id)
+	{
+		$statement = $this->_dbh->prepare("delete from " . $this->_table . " where id = ?");
+		return $statement->execute(array($id));
+	}
+}
